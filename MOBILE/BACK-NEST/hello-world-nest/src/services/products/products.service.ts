3bc383afab11fdc5587f2c0de42ae39c6e935951
Daniel Ibáñez <danizgz95@gmail.com>
2025-10-16 11:47:42 +0200

    [16/10/2025] 11:47

--- MOBILE/BACK-NEST/hello-world-nest/src/services/products/products.service.ts
+++ MOBILE/BACK-NEST/hello-world-nest/src/services/products/products.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+    delete(id: number) {
+ const productFound = this.products.findIndex((item) => item.id === id);
+ if (productFound > 0) {
+ this.products.splice(productFound, 1);
+ }
+}
+update(id: number, payload: any) {
+  const productFound = this.findOne(id);
+  if (productFound != null) {
+    const index = this.products.findIndex((item) => item.id === id);
+    this.products[index] = {
+      ...productFound,
+      ...payload,
+    }
+  }
+}
+
+}
+
